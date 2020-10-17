e5ffb57b7c0be3c9cf59031ba4c8bbb3e8e64465
ItsAllDari <itsalldari@gmail.com>
2020-10-17 11:47:29 -0400

    Updated models file
    Made sure dunder methods were correct for each class
    Imported receiver, pre-save and slugify

--- api/models/models.py
+++ api/models/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.signals import pre_save
 from django.contrib.auth import get_user_model
 from .user import User
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
 class Quiz(models.Model):
   name = models.CharField(max_length=100)
@@ -31,7 +34,7 @@ class Answer (models.Model):
   is_correct = models.BooleanField(default=False)
 
   def __str__(self):
-    return self.question.label
+    return self.label
 
 class Player(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -48,3 +51,10 @@ class PlayerAnswer(models.Model):
   player = models.ForeignKey(Player, on_delete=models.CASCADE)
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.question.label
+
+@receiver(pre_save, sender=Quiz)
+def slugify_name(sender, instance, *args, **kwargs):
+  instance.slug = slugify(instance.name)
